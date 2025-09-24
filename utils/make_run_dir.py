76d6e3942d2aed0b7e209a9d83a60507d96f2f51
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-09-23 23:20:10 -0400

    Make it possible for folder to already exist

--- utils/make_run_dir.py
+++ utils/make_run_dir.py
@@ -17,5 +17,5 @@ def make_run_dir(mode: str, base: str = "runs") -> str:
     ts = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
     run_name = f"{mode}_{ts}"
     run_dir = pathlib.Path(base) / run_name
-    run_dir.mkdir(parents=True, exist_ok=False)
+    run_dir.mkdir(parents=True, exist_ok=True)
     return str(run_dir)
